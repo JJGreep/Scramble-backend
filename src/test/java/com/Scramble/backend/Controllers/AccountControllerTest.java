8c5d7cc77cb65faeedad82190f10872c03a7580e
Jasper Greep <0885087@hr.nl>
2020-01-06 14:32:51 +0100

    Tests corrected and sonar properties file added

--- src/test/java/com/Scramble/backend/Controllers/AccountControllerTest.java
+++ src/test/java/com/Scramble/backend/Controllers/AccountControllerTest.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,93 +31,97 @@ class AccountControllerTest {
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
 
-//    @Test
-//    void testFindAccountById() throws Exception {
-//        //Given
-//        AccountController accountController = new AccountController(accountRepo);
-//        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
-//        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
-//        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
-//        Account account = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
-//        when(accountRepo.findById(0L)).thenReturn(Optional.of(account));
-//
-//        //When
-//        accountController.findAccountById(0L);
-//
-//        //Then
-//        verify(accountRepo).findById(0L);
-//    }
-//
-//    @Test
-//    void testFindAllAccounts() throws Exception{
-//        //Given
-//        AccountController accountController = new AccountController(accountRepo);
-//        List<Account> accountList = new ArrayList<Account>();
-//        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
-//        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
-//        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
-//        Account account1 = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
-//        Account account2 = new Account("test2@test.nl", eatGroups, "Test2", "test2", favRestaurants, history);
-//        accountList.add(account1);
-//        accountList.add(account2);
-//        //when(eatGroupRepo.findByName("PokoLoco")).thenReturn(groupList);
-//        when(accountRepo.findAll()).thenReturn(accountList);
-//
-//        //When
-//        accountController.findAllAccounts("");
-//
-//        //Then
-//        verify(accountRepo).findAll();
-//    }
-//
-//    //TODO: Test not correct
-////    @Test
-////    void testCreateAccount() throws Exception{
-////        //Given
-////        AccountController accountController = new AccountController(accountRepo);
-////        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
-////        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
-////        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
-////
-////        //When
-////        Account account = accountController.createAccount(new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history));
-////
-////        //Then
-////        verify(accountRepo).save(account);
-////    }
-//
-//    @Test
-//    void testUpdateAccount() throws Exception{
-//        //Given
-//        AccountController accountController = new AccountController(accountRepo);
-//        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
-//        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
-//        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
-//        Account account = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
-//        when(accountRepo.findById(0L)).thenReturn(Optional.of(account));
-//
-//        //When
-//        accountController.updateAccount(0L, account);
-//
-//        //Then
-//        verify(accountRepo).save(account);
-//    }
-//
-//    @Test
-//    void testDeleteAccount() throws Exception{
-//        //Given
-//        AccountController accountController = new AccountController(accountRepo);
-//        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
-//        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
-//        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
-//        Account account = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
-//        when(accountRepo.findById(0L)).thenReturn(Optional.of(account));
-//
-//        //When
-//        accountController.deleteAccount(0L);
-//
-//        //Then
-//        verify(accountRepo).delete(account);
-//    }
+    @Test
+    void testFindAccountById() throws Exception {
+        //Given
+        AccountController accountController = new AccountController(accountRepo);
+        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
+        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
+        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
+        Account account = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
+        when(accountRepo.findById(0L)).thenReturn(Optional.of(account));
+
+        //When
+        accountController.findAccountById(0L);
+
+        //Then
+        verify(accountRepo).findById(0L);
+    }
+
+    @Test
+    void testFindAllAccounts() throws Exception{
+        //Given
+        AccountController accountController = new AccountController(accountRepo);
+        List<Account> accountList = new ArrayList<Account>();
+        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
+        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
+        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
+        Account account1 = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
+        Account account2 = new Account("test2@test.nl", eatGroups, "Test2", "test2", favRestaurants, history);
+        accountList.add(account1);
+        accountList.add(account2);
+        //when(eatGroupRepo.findByName("PokoLoco")).thenReturn(groupList);
+        when(accountRepo.findAll()).thenReturn(accountList);
+
+        //When
+        accountController.findAllAccounts("");
+
+        //Then
+        verify(accountRepo).findAll();
+    }
+
+    //TODO: Test not correct
+    @Test
+    void testCreateAccount() throws Exception{
+        //Given
+        AccountController accountController = new AccountController(accountRepo);
+        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
+        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
+        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
+        Account newAccount = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
+
+        when(accountRepo.save(newAccount)).thenReturn(newAccount);
+
+        //When
+        Account account = accountController.createAccount(newAccount);
+
+        //Then
+        verify(accountRepo).save(newAccount);
+        assertThat(account,is(equalTo(newAccount)));
+    }
+
+    @Test
+    void testUpdateAccount() throws Exception{
+        //Given
+        AccountController accountController = new AccountController(accountRepo);
+        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
+        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
+        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
+        Account account = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
+        when(accountRepo.findById(0L)).thenReturn(Optional.of(account));
+
+        //When
+        accountController.updateAccount(0L, account);
+
+        //Then
+        verify(accountRepo).save(account);
+    }
+
+    @Test
+    void testDeleteAccount() throws Exception{
+        //Given
+        AccountController accountController = new AccountController(accountRepo);
+        List<EatGroup> eatGroups = new ArrayList<EatGroup>();
+        List<RestaurantDb> favRestaurants = new ArrayList<RestaurantDb>();
+        List<RestaurantDb> history = new ArrayList<RestaurantDb>();
+        Account account = new Account("test@test.nl", eatGroups, "Test", "test", favRestaurants, history);
+        when(accountRepo.findById(0L)).thenReturn(Optional.of(account));
+
+        //When
+        accountController.deleteAccount(0L);
+
+        //Then
+        verify(accountRepo).delete(account);
+    }
 
 }
--- src/test/java/com/Scramble/backend/Controllers/GroupControllerTest.java
+++ src/test/java/com/Scramble/backend/Controllers/GroupControllerTest.java
@@ -27,78 +27,78 @@ class GroupControllerTest {
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
 
-//    @Test
-//    void testFindById() throws Exception {
-//        //Given
-//        GroupController groupController = new GroupController(eatGroupRepo);
-//        EatGroup group = new EatGroup(0, "Javaanse Strijders");
-//        when(eatGroupRepo.findById(0L)).thenReturn(Optional.of(group));
-//
-//        //When
-//        groupController.findById(0L);
-//
-//        //Then
-//        verify(eatGroupRepo).findById(0L);
-//    }
-//
-//    @Test
-//    void testFindAll() throws Exception{
-//        //Given
-//        GroupController groupController = new GroupController(eatGroupRepo);
-//        List<EatGroup> groupList = new ArrayList<EatGroup>();
-//        EatGroup group1 = new EatGroup(0, "Javaanse Strijders");
-//        EatGroup group2 = new EatGroup(1, "PokoLoco");
-//        groupList.add(group1);
-//        groupList.add(group2);
-//        //when(eatGroupRepo.findByName("PokoLoco")).thenReturn(groupList);
-//        when(eatGroupRepo.findAll()).thenReturn(groupList);
-//
-//        //When
-//        groupController.findAll("");
-//
-//        //Then
-//        verify(eatGroupRepo).findAll();
-//    }
-//
-//    //TODO:Test not correct
-////    @Test
-////    void testCreateGroup() throws Exception{
-////        //Given
-////        GroupController groupController = new GroupController(eatGroupRepo);
-////
-////        //When
-////        EatGroup group = groupController.createGroup(new EatGroup(0L, "Javaanse Strijders"));
-////
-////        //Then
-////        verify(eatGroupRepo).save(group);
-////    }
-//
-//    @Test
-//    void testUpdateGroup() throws Exception{
-//        //Given
-//        GroupController groupController = new GroupController(eatGroupRepo);
-//        EatGroup group = new EatGroup(0, "Javaanse Strijders");
-//        when(eatGroupRepo.findById(0L)).thenReturn(Optional.of(group));
-//
-//        //When
-//        groupController.updateGroup(0L, group);
-//
-//        //Then
-//        verify(eatGroupRepo).save(group);
-//    }
-//
-//    @Test
-//    void testDeleteGroup() throws Exception{
-//        //Given
-//        GroupController groupController = new GroupController(eatGroupRepo);
-//        EatGroup group = new EatGroup(0, "Javaanse Strijders");
-//        when(eatGroupRepo.findById(0L)).thenReturn(Optional.of(group));
-//
-//        //When
-//        groupController.deleteGroup(0L);
-//
-//        //Then
-//        verify(eatGroupRepo).delete(group);
-//    }
+    @Test
+    void testFindById() throws Exception {
+        //Given
+        GroupController groupController = new GroupController(eatGroupRepo);
+        EatGroup group = new EatGroup(0, "Javaanse Strijders");
+        when(eatGroupRepo.findById(0L)).thenReturn(Optional.of(group));
+
+        //When
+        groupController.findById(0L);
+
+        //Then
+        verify(eatGroupRepo).findById(0L);
+    }
+
+    @Test
+    void testFindAll() throws Exception{
+        //Given
+        GroupController groupController = new GroupController(eatGroupRepo);
+        List<EatGroup> groupList = new ArrayList<EatGroup>();
+        EatGroup group1 = new EatGroup(0, "Javaanse Strijders");
+        EatGroup group2 = new EatGroup(1, "PokoLoco");
+        groupList.add(group1);
+        groupList.add(group2);
+        //when(eatGroupRepo.findByName("PokoLoco")).thenReturn(groupList);
+        when(eatGroupRepo.findAll()).thenReturn(groupList);
+
+        //When
+        groupController.findAll("");
+
+        //Then
+        verify(eatGroupRepo).findAll();
+    }
+
+    //TODO:Test not correct
+    @Test
+    void testCreateGroup() throws Exception{
+        //Given
+        GroupController groupController = new GroupController(eatGroupRepo);
+
+        //When
+        EatGroup group = groupController.createGroup(new EatGroup(0L, "Javaanse Strijders"));
+
+        //Then
+        verify(eatGroupRepo).save(group);
+    }
+
+    @Test
+    void testUpdateGroup() throws Exception{
+        //Given
+        GroupController groupController = new GroupController(eatGroupRepo);
+        EatGroup group = new EatGroup(0, "Javaanse Strijders");
+        when(eatGroupRepo.findById(0L)).thenReturn(Optional.of(group));
+
+        //When
+        groupController.updateGroup(0L, group);
+
+        //Then
+        verify(eatGroupRepo).save(group);
+    }
+
+    @Test
+    void testDeleteGroup() throws Exception{
+        //Given
+        GroupController groupController = new GroupController(eatGroupRepo);
+        EatGroup group = new EatGroup(0, "Javaanse Strijders");
+        when(eatGroupRepo.findById(0L)).thenReturn(Optional.of(group));
+
+        //When
+        groupController.deleteGroup(0L);
+
+        //Then
+        verify(eatGroupRepo).delete(group);
+    }
 
 }
