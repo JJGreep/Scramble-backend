059ef3f60423091b505c81498942e7fd37f98a0d
Dines123 <34677908+Dines123@users.noreply.github.com>
2020-01-21 13:35:37 +0100

    Begin from filter

--- src/main/java/com/Scramble/backend/Services/ZomatoService.java
+++ src/main/java/com/Scramble/backend/Services/ZomatoService.java
@@ -1,10 +1,17 @@
 package com.Scramble.backend.Services;
+<<<<<<< Updated upstream
+=======
+import com.Scramble.backend.Models.Cuisine;
+import com.Scramble.backend.Models.CuisineDetails;
+import com.Scramble.backend.Models.CuisineList;
+>>>>>>> Stashed changes
 import com.Scramble.backend.Models.Search;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.Collections;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class ZomatoService {
@@ -44,6 +51,27 @@ public class ZomatoService {
 
     }
 
+<<<<<<< Updated upstream
+=======
+    public List<CuisineDetails> GetAllCuisines() {
+
+        String url = "https://developers.zomato.com/api/v2.1/cuisines?lat=29.054026&lon=-22.557066";
+
+        HttpHeaders headers = new HttpHeaders();
+        RestTemplate restTemplate = new RestTemplate();
+
+        headers.setAccept(Collections.singletonList((MediaType.APPLICATION_JSON)));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("user-key", "a71057b41a6526cec5e3cc053ad34915" );
+
+        HttpEntity<CuisineList> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<CuisineList> response = restTemplate.exchange(url, HttpMethod.GET, entity, CuisineList.class);
+
+        return response.getBody().getCuisines().stream().map(c -> c.getCuisine()).collect(Collectors.toList());
+    }
+
+>>>>>>> Stashed changes
 
 
 
